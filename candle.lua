f4fef629eef8607657a2be012c3cb0074cf20757
alphexus <52839154+Alphexus@users.noreply.github.com>
2021-08-10 15:03:04 -0400

    Create candle.lua

---
+++ candle.lua
@@ -0,0 +1,33 @@
+local Candle = {
+    array = {},
+    dict = {}
+}
+
+function Candle.array.filter(table, predicate)
+    local new = {}
+    for i, v in pairs(table) do
+        if predicate(i, v) then
+            table.insert(new, i, v)
+        end
+    end
+    return new
+end
+
+function Candle.array.concat(table0, table1)
+    return {unpack(table0), unpack(table1)}
+end
+
+function Candle.array.flat(table, depth)
+    if depth == 0 then return table end
+
+    local new = {}    
+    for i, v in pairs(table) do
+        if type(v) == "table" then
+            new = self.array.concat(new, v)
+        else
+            table.insert(new, v)
+        end
+    end
+end
+
+return Candle
